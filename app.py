fb2e8bff457afddcf36ddc573197afc752c0e690
zarubb <zarubb@yandex.ru>
2020-10-01 14:28:34 +0600

    Initial

---
+++ app.py
@@ -0,0 +1,23 @@
+import time
+
+
+slide1 = '(^_^)'
+slide2 = '(^_~)'
+slide3 = '(^_^)'
+slide4 = '(о_О)'
+slide5 = '(O_O)'
+slide6 = 'Hello, World!'
+
+time.sleep(1)
+print(slide1, end='\r')
+time.sleep(1)
+print(slide2, end='\r')
+time.sleep(1)
+print(slide3, end='\r')
+time.sleep(1)
+print(slide4, end='\r')
+time.sleep(1)
+print(slide5, end='\r')
+time.sleep(1)
+print(slide6)
+
